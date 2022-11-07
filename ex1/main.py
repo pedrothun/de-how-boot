0d93a7a3917010a0a988a51d24899e09c79baba1
pedrothun <pedrothun@gmail.com>
2022-11-06 21:08:31 -0300

    calc bola sort freq

--- ex1/main.py
+++ ex1/main.py
@@ -1,3 +1,5 @@
+from pty import master_open
+from xml.dom import NOT_SUPPORTED_ERR
 import requests
 import pandas as pd
 import collections
@@ -24,25 +26,26 @@ df = pd.read_html(r_text)
 #df[0]
 #type(df[0])
 df=df[0].copy()
-df.dtypes
+#df.dtypes
 df.columns
 df.head()
 df = df[df['Bola1'] == df['Bola1']]
 df['Concurso'] = pd.to_numeric(df['Concurso'])
 df.sort_values('Concurso',ascending=True)
+df.head()
 
 df_smzed = df.melt(id_vars='Concurso',value_vars=['Bola1','Bola2','Bola3','Bola4','Bola5','Bola6','Bola7','Bola8','Bola9','Bola10','Bola11','Bola12','Bola13','Bola14','Bola15'],var_name = 'bola_id',value_name = 'bola_num_sort').sort_values(['Concurso','bola_id'],ascending=True)
 df_smzed = df_smzed.reset_index(drop=True)
 df_smzed.dtypes
+df.to_csv('./df.csv',sep=';')
 #df_smzed = df_smzed[df_smzed['Concurso']<=100]
-df_smzed
+df_smzed.to_csv('./df_smzed.csv',sep=';')
 nr_pop = list(range(1, 26))
 nr_pares = list(range(2,26,2))  #[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24]
 nr_impares = list(range(1,27,2)) #[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
 nr_primos = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
 demo = df['Concurso'].unique()
-demo
 comb_concursos = []
 
 for i in demo:
@@ -61,6 +64,15 @@ for i in demo:
     k = ('Concurso-'+str(i)+' '+str(v_pares)+'p-'+str(v_impares)+'i-'+str(v_primos)+'np')
     comb_concursos.append(k)
 
+df_smzed_frequency = df_smzed.groupby('bola_num_sort')['Concurso'].count()
+df_smzed_frequency
+max_concurso = df_smzed['Concurso'].max()
+nr_freq = (df_smzed_frequency*100 / max_concurso).to_frame()
+nr_freq = nr_freq.sort_values('Concurso',ascending = False)
+nr_freq.reset_index(inplace=True)
+nr_freq.columns = ['bola_num_sort','freq']
+nr_freq.iloc[0]['bola_num_sort']
+nr_freq.iloc[-1]['bola_num_sort']
 
 comb = []
 v_01 = 0
@@ -88,10 +100,6 @@ v_22 = 0
 v_23 = 0
 v_24 = 0
 v_25 = 0
-
-lst_campos = ['Bola1', 'Bola2', 'Bola3', 'Bola4', 'Bola5',
-              'Bola6', 'Bola7', 'Bola8', 'Bola9', 'Bola10', 'Bola11', 'Bola12',
-               'Bola13', 'Bola14', 'Bola15']
 
 
 for index, row in df.iterrows():
